fae85c31df971a126d4428fdb07232e0a7c6f321
JungGYUCHEOL <kcpooh0202@naver.com>
2020-08-06 09:07:00 +0900

    springboot hateoas

---
+++ src/main/java/com/example/springboot13/SampleController.java
@@ -0,0 +1,26 @@
+package com.example.springboot13;
+
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public EntityModel<Hello> hello(){
+        Hello hello = new Hello();
+        hello.setPrefix("Hey,");
+        hello.setName("keesun");
+
+        EntityModel<Hello> helloEntityModel = new EntityModel<>(hello);
+        helloEntityModel.add(linkTo(methodOn(SampleController.class).hello()).withSelfRel());
+
+        return helloEntityModel;
+
+    }
+}
